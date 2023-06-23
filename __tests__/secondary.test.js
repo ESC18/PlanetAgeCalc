af2b22d8f36455f55a5dbcd1c5f34327264de1ba
ESC18 <ponderosaprintingus@gmail.com>
2023-06-23 11:44:45 -0700

    Encountered Multiple errors - Errors fixed and tests are being developed (WIP).

--- __tests__/secondary.test.js
+++ __tests__/secondary.test.js
@@ -1,17 +1,16 @@
 // Example of test below complete with import statement.
-/*
-import Blank from './../src/rectangle.js';
+
+import Blank from './../src/js/secondary';
 
 describe("Blank", () => {
     let blank; 
     
     beforeEach(() => {
-        blank = new Blank(x,y);
+        blank = new Blank();
     })
 
     test("blah blah blah blah blah", () => {
-        expect(Blank.x).toEqual(xx);
-        expect(Blank.y).toEqual(yy);
+        expect(blank.x).toEqual();
+        expect(blank.y).toEqual();
     });
 })
-*/
