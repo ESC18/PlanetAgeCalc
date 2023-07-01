3891573b781e59b76c2398fa5225ce1abc0aab60
ESC18 <ponderosaprintingus@gmail.com>
2023-07-01 15:58:09 -0700

    Complete project - finishes UI Finishes UI and Business Logic

--- src/index.js
+++ src/index.js
@@ -2,25 +2,65 @@
 import './assets/images/SC.png';
 import './assets/fonts.css';
 import './css/styles.css';
-//import personsAge from './js/secondary'
-//EX: import Blank from '/.js/secondary.js';
+import personsAge from './js/secondary';
+// Business logic 
+function calculateAge(selection, currentAge, pastAge, futureAge) {
+    const age = new personsAge(currentAge);
 
-// function ageCalc(age,secondAge,futureAge) {
+    const result = {};
 
-// }
+    switch (selection) {
+    case 'present':
+        result['Earth'] = age.earthAge();
+        result['Mercury'] = age.mercuryAge();
+        result['Venus'] = age.venusAge();
+        result['Mars'] = age.marsAge();
+        result['Jupiter'] = age.jupiterAge();
+        break;
+    case 'future':
+        result['Earth'] = age.earthFutureYears(futureAge);
+        result['Mercury'] = age.mercuryFutureYears(futureAge);
+        result['Venus'] = age.venusFutureYears(futureAge);
+        result['Mars'] = age.marsFutureYears(futureAge);
+        result['Jupiter'] = age.jupiterFutureYears(futureAge);
+        break;
+    case 'past':
+        result['Earth'] = age.earthPassedYears(pastAge);
+        result['Mercury'] = age.mercuryPassedYears(pastAge);
+        result['Venus'] = age.venusPassedYears(pastAge);
+        result['Mars'] = age.marsPassedYears(pastAge);
+        result['Jupiter'] = age.jupiterPassedYears(pastAge);
+        break;
+    default:
+        result['Error'] = 'Invalid option selected';
+    }
 
-
-
-function submitFunc (event) {
-    event.preventDefault();
-    const selection = document.querySelector("select").value;
-    const ageForm = document.querySelector("form");
-    
-    console.log(selection, ageForm);
-
+    return result;
 }
 
 
+// UI logic
+function displayResult(result) {
+    const resultElement = document.getElementById('result');
+    let resultHTML = '';
+
+    for (const planet in result) {
+        resultHTML += `<p>${planet}: ${result[planet]}</p>`;
+    }
+
+    resultElement.innerHTML = resultHTML;
+}
+
+function submitFunc(event) {
+    event.preventDefault();
+    const selection = document.querySelector("select").value;
+    const currentAge = document.getElementById("current").value;
+    const pastAge = document.getElementById("past").value;
+    const futureAge = document.getElementById("future").value;
+
+    const result = calculateAge(selection, currentAge, pastAge, futureAge);
+    displayResult(result);
+}
 
 window.addEventListener("load", function () {
     document.querySelector("form").addEventListener("submit", submitFunc);
--- src/js/secondary.js
+++ src/js/secondary.js
@@ -15,56 +15,54 @@ export default class Blank {
 */
 
 export default class personsAge {
-    constructor(age, secondAge, futureAge) {
+    constructor(age) {
         this.age = age;
-        this.secondAge = secondAge;
-        this.futureAge = futureAge;
     }
     earthAge() {
-        return parseInt(this.age / 1) + " Years Old";
+        return parseInt(this.age / 1) + ' Years Old';
     }
     mercuryAge() {
-        return parseFloat(this.age / 0.24) + " Years Old";
+        return parseFloat(this.age / 0.24) + ' Years Old';
     }
     venusAge() {
-        return parseFloat(this.age / 0.62) + " Years Old";
+        return parseFloat(this.age / 0.62) + ' Years Old';
     }
     marsAge() {
-        return parseFloat(this.age / 1.88) + " Years Old";
+        return parseFloat(this.age / 1.88) + ' Years Old';
     }
     jupiterAge() {
-        return parseFloat(this.age / 11.86) + " Years Old";
+        return parseFloat(this.age / 11.86) + ' Years Old';
     }
-    //Methods used to determine difference in years
-    earthPassedYears() {
-        return parseInt(this.secondAge / 1) - parseInt(this.age / 1) + " Years have passed";
+    // Methods used to determine difference in years
+    earthPassedYears(secondAge) {
+        return parseInt(this.age / 1) - parseInt(secondAge / 1) + ' Years have passed';
     }
-    mercuryPassedYears() {
-        return parseFloat(this.secondAge / 0.24) - parseFloat(this.age / 0.24) + " Years have passed";
+    mercuryPassedYears(secondAge) {
+        return parseFloat(this.age / 0.24) - parseFloat(secondAge / 0.24) + ' Years have passed';
     }
-    venusPassedYears() {
-        return parseFloat(this.secondAge / 0.62) - parseFloat(this.age / 0.62) + " Years have passed";
+    venusPassedYears(secondAge) {
+        return parseFloat(this.age / 0.62) - parseFloat(secondAge / 0.62) + ' Years have passed';
     }
-    marsPassedYears() {
-        return parseFloat(this.secondAge / 1.88) - parseFloat(this.age / 1.88) + " Years have passed";
+    marsPassedYears(secondAge) {
+        return parseFloat(this.age / 1.88) - parseFloat(secondAge / 1.88) + ' Years have passed';
     }
-    jupiterPassedYears() {
-        return parseFloat(this.secondAge / 11.86) - parseFloat(this.age / 11.86) + " Years have passed";
+    jupiterPassedYears(secondAge) {
+        return parseFloat(this.age / 11.86) - parseFloat(secondAge / 11.86) + ' Years have passed';
     }
-    //Methods used to determine difference in years for present and future years
-    earthFutureYears() {
-        return parseInt(this.futureAge / 1) - parseInt(this.age / 1) + " Years to go";
+    // Methods used to determine difference in years for present and future years
+    earthFutureYears(futureAge) {
+        return parseInt(futureAge / 1) - parseInt(this.age / 1) + ' Years to go';
     }
-    mercuryFutureYears() {
-        return parseFloat(this.futureAge / 0.24) - parseFloat(this.age / 0.24) + " Years to go";
+    mercuryFutureYears(futureAge) {
+        return parseFloat(futureAge / 0.24) - parseFloat(this.age / 0.24) + ' Years to go';
     }
-    venusFutureYears() {
-        return parseFloat(this.futureAge / 0.62) - parseFloat(this.age / 0.62) + " Years to go";
+    venusFutureYears(futureAge) {
+        return parseFloat(futureAge / 0.62) - parseFloat(this.age / 0.62) + ' Years to go';
     }
-    marsFutureYears() {
-        return parseFloat(this.futureAge / 1.88) - parseFloat(this.age / 1.88) + " Years to go";
+    marsFutureYears(futureAge) {
+        return parseFloat(futureAge / 1.88) - parseFloat(this.age / 1.88) + ' Years to go';
     }
-    jupiterFutureYears() {
-        return parseFloat(this.futureAge / 11.86) - parseFloat(this.age / 11.86) + " Years to go";
+    jupiterFutureYears(futureAge) {
+        return parseFloat(futureAge / 11.86) - parseFloat(this.age / 11.86) + ' Years to go';
     }
 }
